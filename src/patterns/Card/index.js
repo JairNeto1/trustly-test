ede09304e7712c0b2ff85d73d88438881d6ae9b5
Ackyla Ferreira Batista <ackylafb@gmail.com>
2020-11-05 02:45:33 -0300

    inverted select fields

--- src/patterns/Card/index.js
+++ src/patterns/Card/index.js
@@ -21,27 +21,27 @@ export default function Card({ product }) {
         <section>
           <label htmlFor="size">Size</label>
           <select name="size" id="size">
+            <option value="34">34</option>
+            <option value="35">35</option>
+            <option value="36">36</option>
+            <option value="37">37</option>
+            <option value="38">38</option>
+            <option value="39">39</option>
+            <option value="40">40</option>
+            <option value="41">41</option>
+            <option value="42">42</option>
+            <option value="43">43</option>
+            <option value="44">44</option>
+          </select>
+          
+          <label htmlFor="quantity">Quantity</label>
+          <select name="quantity" id="quantity">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
           </select>
-          
-          <label htmlFor="quantity">Quantity</label>
-          <select name="quantity" id="quantity">
-              <option value="34">34</option>
-              <option value="35">35</option>
-              <option value="36">36</option>
-              <option value="37">37</option>
-              <option value="38">38</option>
-              <option value="39">39</option>
-              <option value="40">40</option>
-              <option value="41">41</option>
-              <option value="42">42</option>
-              <option value="43">43</option>
-              <option value="44">44</option>
-            </select>
         </section>
 
         <span>$ {product.price}</span>
